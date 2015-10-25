bb11b9ea06410ea8639a97e6b28465e2f7e5e4bf
Alex Boklin <alex.boklin@gmail.com>
2015-10-25 22:01:45 +0300

    Update Coin_Change.hs

--- Coin_Change.hs
+++ Coin_Change.hs
@@ -4,5 +4,5 @@ coins = [2, 3, 7]
 change :: (Ord a, Num a) => a -> [[a]]
 
 change n = if n < minimum coins then [] else change' n where
-	change' m	| m < minimum coins = [[]]
-				| otherwise = [ xs | x <- coins, x <= m, xs <- map (\ys -> x:ys) $ change' (m - x), sum xs == m ]     
+	change' m  | m < minimum coins = [[]]
+		   | otherwise = [ xs | x <- coins, x <= m, xs <- map (\ys -> x:ys) $ change' (m - x), sum xs == m ]     
